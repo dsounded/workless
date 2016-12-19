7da86ae50dc6388a88f3ec31fd280336c3243a6c
Kiril Dokh <kiril.dokh@onapp.com>
2016-12-19 14:44:07 +0200

    Rails 5 deprecation message

--- lib/workless/scaler.rb
+++ lib/workless/scaler.rb
@@ -11,9 +11,21 @@ module Delayed
         base.send :extend, ClassMethods
         if base.to_s =~ /ActiveRecord/
           base.class_eval do
-            after_commit "self.class.scaler.down", :on => :update, :if => Proc.new {|r| !r.failed_at.nil? }
-            after_commit "self.class.scaler.down", :on => :destroy, :if => Proc.new {|r| r.destroyed? or !r.failed_at.nil? }
-            after_commit "self.class.scaler.up", :on => :create
+            # def on_destroy
+            # def on_update
+            %i(on_destroy on_update).each do |method_name|
+              define_method method_name do
+                self.class.scaler.down
+              end
+            end
+
+            define_method(:on_create) do
+              self.class.scaler.up
+            end
+
+            after_commit :on_destroy, :on => :update, :if => Proc.new {|r| !r.failed_at.nil? }
+            after_commit :on_create, :on => :destroy, :if => Proc.new {|r| r.destroyed? or !r.failed_at.nil? }
+            after_commit :on_update, :on => :create
           end          
         elsif base.to_s =~ /Sequel/
           base.send(:define_method, 'after_destroy') do
